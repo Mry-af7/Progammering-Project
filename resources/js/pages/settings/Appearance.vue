4bd3d6ce8272f09e08a44b3fe9b1ba39be5fd470
Amina0506 <amina010506@gmail.com>
2025-06-10 16:00:20 +0200

    Vertaling van de profielpagina naar Nederlands

--- resources/js/pages/settings/Appearance.vue
+++ resources/js/pages/settings/Appearance.vue
@@ -10,7 +10,7 @@ import SettingsLayout from '@/layouts/settings/Layout.vue';
 
 const breadcrumbItems: BreadcrumbItem[] = [
     {
-        title: 'Appearance settings',
+        title: 'Verschijningsinstellingen',
         href: '/settings/appearance',
     },
 ];
@@ -18,11 +18,11 @@ const breadcrumbItems: BreadcrumbItem[] = [
 
 <template>
     <AppLayout :breadcrumbs="breadcrumbItems">
-        <Head title="Appearance settings" />
+        <Head title="Verschijningsinstellingen" />
 
         <SettingsLayout>
             <div class="space-y-6">
-                <HeadingSmall title="Appearance settings" description="Update your account's appearance settings" />
+                <HeadingSmall title="Verschijningsinstellingen" description="De weergave-instellingen van je account bijwerken" />
                 <AppearanceTabs />
             </div>
         </SettingsLayout>
