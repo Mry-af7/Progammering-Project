2b2eca038c73f05da40c7bf50898652cf9f139d7
amizer05 <amine.zerouali@student.ehb.be>
2025-06-11 09:35:26 +0200

    favorietenpagina

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -27,9 +27,13 @@
                       <button class="bg-orange-500 text-white px-6 py-2 rounded-full font-medium hover:bg-orange-600 transition-colors">
                           Info
                       </button>
-                      <button class="bg-orange-500 text-white px-6 py-2 rounded-full font-medium hover:bg-orange-600 transition-colors">
-                          Favorieten
-                      </button>
+                      <Link 
+                       href="/favorieten" 
+                        class="bg-orange-500 text-white px-6 py-2 rounded-full font-medium hover:bg-orange-600 transition-colors inline-block text-center"
+                      >
+                      Favorieten
+                      </Link>
+
                       <!-- Contact link via Inertia -->
                       <Link 
                         href="/contact" 
